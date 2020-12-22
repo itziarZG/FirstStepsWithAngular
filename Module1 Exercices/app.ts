473f3e2f6c47d51650f1fc6b44ac0e72c5b9aeff
Itziar Zameza García <itziar.zameza@gmail.com>
2020-12-22 18:02:46 +0100

    add first exercicies

--- Module1 Exercices/app.ts
+++ Module1 Exercices/app.ts
@@ -1,38 +1,39 @@
-(()=>{
-
+(() => {
   // Uso de Let y Const
-  var nombre = 'Ricardo Tapia';
-  var edad = 23;
+  const nombre = "Ricardo Tapia";
+  const edad = 23;
 
-  var PERSONAJE = {
-    nombre: nombre,
-    edad: edad
-  };
-
+  // const PERSONAJE = {
+  //   nombre: nombre,
+  //   edad: edad,
+  // };
+  const PERSONAJE = { nombre, edad };
 
   // Cree una interfaz que sirva para validar el siguiente objeto
-  var batman = {
-    nombre: 'Bruno Díaz',
-    artesMarciales: ['Karate','Aikido','Wing Chun','Jiu-Jitsu']
+  interface Batman {
+    nombre: string;
+    artesMarciales: string[];
   }
+  const batman: Batman = {
+    nombre: "Bruno Díaz",
+    artesMarciales: ["Karate", "Aikido", "Wing Chun", "Jiu-Jitsu"],
+  };
 
   // Convertir esta funcion a una funcion de flecha
-  function resultadoDoble( a, b ){
-    return (a + b) * 2
-  }
+  const resultadoDoble = (a: number, b: number): number => (a + b) * 2;
 
   // Función con parametros obligatorios, opcionales y por defecto
   // donde NOMBRE = obligatorio
   //       PODER  = opcional
   //       ARMA   = por defecto = 'arco'
-  function getAvenger( nombre, poder, arma ){
+  function getAvenger(nombre: string, arma: string = "arco", poder?: string) {
     var mensaje;
-    if( poder ){
-      mensaje = nombre + ' tiene el poder de: ' + poder + ' y un arma: ' + arma;
-    }else{
-      mensaje = nombre + ' tiene un ' + poder
+    if (poder) {
+      mensaje = nombre + " tiene el poder de: " + poder + " y un arma: " + arma;
+    } else {
+      mensaje = nombre + " tiene un " + poder;
     }
-  };
+  }
 
   // Cree una clase que permita manejar la siguiente estructura
   // La clase se debe de llamar rectangulo,
@@ -41,7 +42,11 @@
   //   * altura
   // También un método que calcule el área  =  base * altura,
   // ese método debe de retornar un numero.
-
-
-
+  class rectangulo {
+    constructor(public altura: number, public base: number) {}
+    area() {
+      return this.base * this.altura;
+    }
+  }
+  //tmb calcularArea = ():number => this.base * this.altura
 })();
